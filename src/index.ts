c3951fefa64d3a837c846fdcfce30154c2a58e06
HoangAnh-it <vha31102002@gmail.com>
2023-11-08 10:54:21 +0700

    add more types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Fuzzer from "./fuzzing";
 import HttpMessageManager from "./httpMessage/HTTPRequestManager"
+import { FuzzingLocationsAlias } from "./httpMessage/constants";
 
 const httpMessageManager = new HttpMessageManager()
 httpMessageManager.setRequestsByFile("src/fuzzing/resources/rawRequests.txt", '~~~')
@@ -10,6 +11,8 @@ fuzz.setHTTPRequestManager(httpMessageManager)
 fuzz.autoDetectFuzzLocation().then(() => {
     for (const req of httpMessageManager.getHTTPRequests()) {
         console.log(req.getStartLine().url.href)
+        console.log("PATH => ",req.getFuzzingLocation(FuzzingLocationsAlias.PATH));
+        console.log("QUERY => ",req.getFuzzingLocation(FuzzingLocationsAlias.QUERY));
         console.log("--------")
     }
 }).catch(console.log)
